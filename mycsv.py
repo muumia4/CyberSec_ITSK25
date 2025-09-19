48e9be217c251194b5adbd45ff8d5ed5d902e22a
muumia4 <muumia4@gmail.com>
2025-09-19 13:12:58 +0300

    Delete mycsv.py

--- mycsv.py
+++
@@ -1,7 +0,0 @@
-filename = 'data/Create-MyCSV-v.csv'
-column = 0 #Veerg mida kokku liita
-total = 0 #Veeru summa
-
-with open(filename, 'r') as f: #Avan faili (with open()) ja loen sisu ('r') ja sellest saab muutuja (as f)
-    contents = f.readlines() #Loeb faili sisu muutujasse
-    print(contents)
