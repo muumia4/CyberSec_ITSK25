f6614cf78e46bb852a8a4bc6747b91f215071844
muumia4 <muumia4@gmail.com>
2025-10-10 18:27:46 +0300

    vastik

--- KoduYL/ITSK25_Stef_reader.py
+++ KoduYL/ITSK25_Stef_reader.py
@@ -1,8 +1,51 @@
-"""
-Andmete analüsaator
+src = "output.txt"
 
-Loo programm, mis loeb eelmise skripti väljundit (output.txt) ning arvutab
-iga rea ja veeru summa. Kogu tulemus tuleb konsooli, seega esmalt näitab
-kuidas numbrid on failis ning siis info rea summade kohta ja veeru summade
-kohta. Lõppu näitab leitud suurim ja väikseim number.
-"""
+#Kuvan faili sisu
+with open(src, "r", encoding="utf-8") as f:
+    content = f.read()
+print("Maatriks:\n" + content)
+
+#Loen sisse maatriksi
+with open(src, "r", encoding="utf-8") as f:
+    rows = f.readlines()
+    print("Rea summad:")
+
+#Korrastan ja loon veergude jaoks tühja listi
+rowCount = 0
+firstRow = rows[0].strip().split()
+columnTotal = [0] * len(firstRow)
+
+firstValue = int(firstRow[0])
+minValue = firstValue
+maxValue = firstValue
+
+#Korrastan info
+for row in rows:
+    row = row.strip()
+    numbers = row.split()
+
+    #Arvutan rea summad
+    rowTotal = 0
+    rowCount += 1
+
+    for i in range(len(numbers)):
+        value = int(numbers[i])
+        rowTotal += value
+        columnTotal[i] += value
+
+        #Jälitan suurimat ja väikseimat arvu
+        if value < minValue:
+            minValue = value
+        if value > maxValue:
+            maxValue = value
+
+    print("Rida " + str(rowCount) + ": " + str(rowTotal))
+
+#Arvutan veeru summad
+print("\nVeeru summad:")
+for x in range(len(columnTotal)):
+    print("Veerg " + str(x + 1) + ": " + str(columnTotal[x]))
+
+#Kuvan suurima ja väikseima arvu
+print("\nSuurim arv: ", maxValue)
+print("Väikseim arv: ", minValue)
